6a82556ee1ca278f97306bd688e1b94a44fe69a4
Gustavo Gualda <104951600+iCrowleySHR@users.noreply.github.com>
2025-04-29 23:49:07 -0300

    fix: centralizando texto

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -6,7 +6,7 @@ defineProps({
 
 <template>
   <div class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-    <p class="font-normal text-gray-700 dark:text-gray-400">{{ msg }}</p>
+    <p class="font-normal text-gray-700 dark:text-gray-400 text-center">{{ msg }}</p>
   </div>
 </template>
 
